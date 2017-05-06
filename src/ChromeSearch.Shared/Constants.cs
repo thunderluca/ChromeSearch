6d90d02b24ab65487aa02f7e9f889483673856ef
Luca Montanari <thunder_luca@fastwebnet.it>
2017-05-06 09:15:59 +0200

    Added UWP and Shared project

---
+++ src/ChromeSearch.Shared/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChromeSearch.Shared
+{
+    public static class Constants
+    {
+        public const string AndroidChromeUserAgent = "Mozilla/5.0 (Linux; Android 6.0.1; SM-G928F Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36";
+    }
+}
